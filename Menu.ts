dc9ff7b451473add714980e95401db7ab489cbf0
Agataandrade <agatasouandrade@gmail.com>
2025-07-11 18:43:05 -0300

    mensagem explicando o que foi feito

--- Menu.ts
+++ Menu.ts
@@ -1,22 +1,15 @@
 import readlinesync = require("readline-sync");
 import { colors } from './src/util/Colors';
-import { Conta } from "./src/model/conta";
-import { ContaCorrente } from "./src/model/contaCorrente";
-import { ContaPoupanca } from "./src/model/contaPoupanca";
+import { ContaCorrente } from "./src/model/ContaCorrente";
+import { ContaPoupanca } from "./src/model/ContaPoupanca";
+
+
 
 export function main() {
 
-    let opcao: number;
-    
-   // Objeto da Classe Conta (Teste)
-    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
-    conta.visualizar();
-    conta.sacar(10500);
-    conta.visualizar();
-    conta.depositar(5000);
-    conta.visualizar();
+     let opcao: number;
 
-   // Objeto da Classe ContaCorrente (Teste)
+    // Objeto da Classe ContaCorrente (Teste)
     const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
     contacorrente.visualizar();
     contacorrente.sacar(2000);
@@ -31,6 +24,7 @@ export function main() {
     contapoupanca.visualizar();
     contapoupanca.depositar(1000);
     contapoupanca.visualizar();
+
 
     while (true) {
 
